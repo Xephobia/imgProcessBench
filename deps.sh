1020aab2e3275a244bf67858536e7986ad801ea6
Xephobia <nathanvalletmarseille@gmail.com>
2020-06-24 08:15:20 +0200

    fix error and add deb packages

--- deps.sh
+++ deps.sh
@@ -2,7 +2,8 @@
 
 # install hyperfine
 cd deps
-curl -o hyperfine.deb https://github.com/sharkdp/hyperfine/releases/download/v1.10.0/hyperfine_1.10.0_amd64.deb
 sudo dpkg -i hyperfine.deb
 # install libvips
-sudo apt install -y libvips-tools
+sudo apt install -y libvips-tools graphicsmagick
+# install gmic
+sudo dpkg -i gmic.deb
