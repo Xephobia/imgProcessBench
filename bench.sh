ce0e6fd5117f9d5a4a5ccbb977a6fe8e10e9ec22
Xephobia <nathanvalletmarseille@gmail.com>
2020-07-01 18:22:02 +0200

    it should work now

--- bench.sh
+++ bench.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 # libvips
-hyperfine --prepare 'sync; echo 3 | sudo tee /proc/sys/vm/drop_cache' 'vips invert img/input/wikipedia.png img/output/wikipedia_inv_vips.jpg'
-
+hyperfine -w 3
 # graphicsmagick
-hyperfine --prepare 'sync; echo 3 | sudo tee /proc/sys/vm/drop_cache' 'gm convert img/input/wikipedia.png -negate img/output/wikpedia_inv_gm.jpg'
+hyperfine -w 3 'gm convert img/input/wikipedia.png -negate img/output/wikpedia_inv_gm.jpg'
 
 # imagemagick
-hyperfine --prepare 'sync; echo 3 | sudo tee /proc/sys/vm/drop_cache' 'deps/magick/convert img/input/wikipedia.png -negate img/output/wikpedia_inv_gm.jpg'
+hyperfine -w 3 'deps/magick/convert img/input/wikipedia.png -negate img/output/wikpedia_inv_gm.jpg'
 
 # gmic
-hyperfine --prepare 'sync; echo 3 | sudo tee /proc/sys/vm/drop_cache' 'gmic img\input\wikipedia.png +negate output img\output\wikipedia_inv_gmic.jpg'
+hyperfine -w 3 'gmic img\input\wikipedia.png +negate output img\output\wikipedia_inv_gmic.jpg'
