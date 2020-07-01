2a137cabbf73c998a7ab06450042610c2e76d62b
Xephobia <nathanvalletmarseille@gmail.com>
2020-07-01 18:26:19 +0200

    i want to see wats wrong

--- bench.sh
+++ bench.sh
@@ -3,10 +3,10 @@
 # libvips
 hyperfine -w 3
 # graphicsmagick
-hyperfine -w 3 'gm convert img/input/wikipedia.png -negate img/output/wikpedia_inv_gm.jpg'
+hyperfine -w 3 --show-output 'gm convert img/input/wikipedia.png -negate img/output/wikpedia_inv_gm.jpg'
 
 # imagemagick
-hyperfine -w 3 'deps/magick/convert img/input/wikipedia.png -negate img/output/wikpedia_inv_gm.jpg'
+hyperfine -w 3 --show-output 'deps/magick/convert img/input/wikipedia.png -negate img/output/wikpedia_inv_gm.jpg'
 
 # gmic
-hyperfine -w 3 'gmic img\input\wikipedia.png +negate output img\output\wikipedia_inv_gmic.jpg'
+hyperfine -w 3 --show-output 'gmic img\input\wikipedia.png +negate output img\output\wikipedia_inv_gmic.jpg'
