46545c7ca0a44be71a9b00671b8931874b36c315
Xephobia <nathanvalletmarseille@gmail.com>
2020-07-02 12:02:44 +0200

    fix path on gm benchmark, it should definitly work now!

--- bench.sh
+++ bench.sh
@@ -4,7 +4,7 @@
 hyperfine -w 3 'vips invert img/input/wikipedia.png img/output/wikipedia_inv_vips.jpg'
 
 # graphicsmagick
-hyperfine -w 3 'gm convert img\input\wikipedia.png -negate img\input\wikipedia_inv_gm.jpg'
+hyperfine -w 3 'gm convert img/input/wikipedia.png -negate img/input/wikipedia_inv_gm.jpg'
 
 # imagemagick
 hyperfine -w 3 'convert img/input/wikipedia.png -negate img/output/wikpedia_inv_gm.jpg'
