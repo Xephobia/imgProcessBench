176af4a1e9b2a7c135f775af6bb5cd4e0e1124e2
Xephobia <nathanvalletmarseille@gmail.com>
2020-07-02 11:22:11 +0200

    stuff

--- bench.sh
+++ bench.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # libvips
-hyperfine -w 3
+
 # graphicsmagick
-hyperfine -w 3 --show-output 'gm convert img/input/wikipedia.png -negate img/output/wikpedia_inv_gm.jpg'
+hyperfine -w 3 --show-output 'gm convert -negate img/output/wikpedia_inv_gm.jpg'
 
 # imagemagick
-hyperfine -w 3 --show-output 'deps/magick/convert img/input/wikipedia.png -negate img/output/wikpedia_inv_gm.jpg'
+hyperfine -w 3 --show-output 'convert img/input/wikipedia.png -negate img/output/wikpedia_inv_gm.jpg'
 
 # gmic
-hyperfine -w 3 --show-output 'gmic img\input\wikipedia.png +negate output img\output\wikipedia_inv_gmic.jpg'
+hyperfine -w 3 --show-output 'gmic img/input/wikipedia.png +negate output img/output/wikipedia_inv_gmic.jpg'
--- deps.sh
+++ deps.sh
@@ -6,4 +6,4 @@ sudo dpkg -i hyperfine.deb
 # install libvips graphicsMagick and gmic
 sudo add-apt-repository ppa:otto-kesselgulasch/gimp
 sudo apt-get update
-sudo apt-get install -y libvips-tools graphicsmagick gmic
+sudo apt-get install -y libvips-tools graphicsmagick gmic imagemagick
