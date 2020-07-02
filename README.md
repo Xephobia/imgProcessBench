4f1993171b45579ab6eaaaa317bd9ce23b0e9345
Xephobia <nathanvalletmarseille@gmail.com>
2020-07-02 12:16:21 +0200

    specify numbers of runs to make and update readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # imgProcessBench
 benchmarks of different image prosessing lib, for personal purpose but you can use that if you want
 
-for now it's uninished with lots of idiot error
+i implemented 4 image prosessing tools benchmark. for result check runs on github action in the benchmark tab.
--- bench.sh
+++ bench.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # libvips
-hyperfine -w 3 'vips invert img/input/wikipedia.png img/output/wikipedia_inv_vips.jpg'
+hyperfine -w 3 -r 10 'vips invert img/input/wikipedia.png img/output/wikipedia_inv_vips.jpg'
 
 # graphicsmagick
-hyperfine -w 3 'gm convert img/input/wikipedia.png -negate img/input/wikipedia_inv_gm.jpg'
+hyperfine -w 3 -r 10 'gm convert img/input/wikipedia.png -negate img/input/wikipedia_inv_gm.jpg'
 
 # imagemagick
-hyperfine -w 3 'convert img/input/wikipedia.png -negate img/output/wikpedia_inv_gm.jpg'
+hyperfine -w 3 -r 10 'convert img/input/wikipedia.png -negate img/output/wikpedia_inv_gm.jpg'
 
 # gmic
-hyperfine -w 3 'gmic img/input/wikipedia.png -negate -output img/output/wikipedia_inv_gmic.jpg'
+hyperfine -w 3 -r 10 'gmic img/input/wikipedia.png -negate -output img/output/wikipedia_inv_gmic.jpg'
